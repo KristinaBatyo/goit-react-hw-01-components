ce4c4d42a6f488e7bd21b2a2cff2069f7bd849b9
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2022-12-01 21:49:16 +0200

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { User } from "./user/User";
+import users from "./profile.json";
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  return ( 
+  <div>
+    <User user={users}/>
+  </div>
   );
 };
